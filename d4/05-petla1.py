6ed320675d88b439585b580ea863fe3b8450b0a1
KalinowA <a.e.kalinowska@gmail.com>
2018-10-09 21:31:26 +0200

    Dzieñ3 - cwiecznia

--- d4/05-petla1.py
+++ d4/05-petla1.py
@@ -13,3 +13,22 @@ while i < len(nazwisko):
     print(nazwisko[i].upper())
     i += 1
 # Inkrementacja: tak samo jak i = i + 1
+
+
+rasa = "Golden River"
+
+i = 0
+
+while i < len(rasa):
+    print(rasa[i].lower())
+    i += 1
+
+
+owoc = "CZERWONE JABLUSZKO"
+
+i = 0
+
+while i < len(owoc):
+    print(owoc[i].lower())
+    i += 1
+
